49a18d63656e10647d737c7215749bfafed8db5c
Red <crabmarket56@gmail.com>
2022-07-20 10:24:09 -0400

    Create array of valid word selections

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,11 @@
 puts 'Hangman initialized'
 
-contents = File.read('../google-10000-english-no-swears.csv')
-puts contents
+possible_words = []
+lines = File.readlines('../google-10000-english-no-swears.csv')
+lines.each do |line|
+  if line.length >= 5 && line.length <= 12
+    possible_words.push(line)
+  end
+end
+puts possible_words.length
+
