4cc47b4b435dceb774d6f5c482ea4abaa59195be
Red <crabmarket56@gmail.com>
2022-07-20 10:37:17 -0400

    Add computer class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,19 @@
-puts 'Hangman initialized'
-
-possible_words = []
-lines = File.readlines('../google-10000-english-no-swears.csv')
-lines.each do |line|
-  if line.length >= 5 && line.length <= 12
-    possible_words.push(line)
+class Computer
+  puts 'Hangman initialized'
+  @@possible_words = []
+  def start_game
+    lines = File.readlines('../google-10000-english-no-swears.csv')
+    lines.each do |line|
+      if line.length >= 5 && line.length <= 12
+      @@possible_words.push(line)
+    end
+  end
+  answer = @@possible_words.sample
+  answer.length.times do
+    puts '_'
+    end
   end
 end
-puts possible_words.length
 
+computer_player = Computer.new
+computer_player.start_game
