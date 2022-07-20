81c0ce85f40ac2f7a5efb3dccd5d84cd757bd48b
Red <crabmarket56@gmail.com>
2022-07-20 11:53:48 -0400

    Add correct guesses array

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,27 +1,28 @@
 class Player
-@@answer = []
-
-end
-
-class Computer < Player
-  def start_game
-    possible_words = []
-    puts 'Hangman initialized'
-    lines = File.readlines('../google-10000-english-no-swears.csv')
-    lines.each do |line|
-      possible_words.push(line) if line.length >= 5 && line.length <= 12
-    end
-    string_answer = possible_words.sample
-    string_answer.length.times do
-      puts '_'
-    end
-    @@answer = string_answer.split(//) 
+  @@answer = []
+  @@correct_guesses = []
   end
-end
-
-class Human < Player
-
-end
-
-computer_player = Computer.new
-computer_player.start_game
+  
+  class Computer < Player
+    def start_game
+      possible_words = []
+      puts 'Hangman initialized'
+      lines = File.readlines('../google-10000-english-no-swears.csv')
+      lines.each do |line|
+        possible_words.push(line) if line.length >= 5 && line.length <= 13
+      end
+      string_answer = possible_words.sample.gsub("\n", "")
+      string_answer.length.times do
+        @@correct_guesses.push('_')
+      end
+      p @@correct_guesses
+      p @@answer = string_answer.split(//) 
+    end
+  end
+  
+  class Human < Player
+  
+  end
+  
+  computer_player = Computer.new
+  computer_player.start_game
